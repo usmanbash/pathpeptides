f0075be17ed027d68b8efaf8c0b385c0e93ecdd1
usmanbash91 <usman@nine15.com>
2025-09-15 01:19:50 +0500

    3

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-  const swiperHero = new Swiper(".swiperSliderHero", {
-    loop: true, // Infinite loop
-    slidesPerView: 1,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    autoplay: {
-      enabled: false,
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    speed: 800,
-  });
-
-  // Products Swiper
   
-  const swiperProducts = new Swiper(".mySwiperProducts", {
-    "direction": "horizontal",
-    "effect": "slide",
-    "loop": false,
-    "speed": 500,
-    "spaceBetween": 20,
-    "slidesPerGroup": 1,
-    "initialSlide": 0,
-    "autoHeight": false,
-    "slideToClickedSlide": false, 
-    "grabCursor": false,
-    "navigation": {
-      "prevEl": ".swiper-button-prev",
-      "nextEl": ".swiper-button-next"
-    },
-    "freeMode": false,
-    "keyboard": false,
-    "scrollbar": false,
-    "zoom": false,
-    "hashNavigation": false,
-    "history": false,
-    "a11y": true,
-    breakpoints: {
-      320: {
-        slidesPerView: 1.2,
-      },
-      630: {
-        slidesPerView: 2.2,
-      },
-      991: {
-        slidesPerView: 3.03,
-      }
-    }
-  });
 });
   
 
